d8018c2e0bac4ced68917b318ebf3c1d7d8122e8
Diego <diego.a.pachecos@gmail.com>
2024-06-21 21:37:37 -0400

    se agrega pagina
    Se añade el archivo html y css de brutalismoAPI, correspondiendo a una API.

--- web/urls.py
+++ web/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import index, info, portafolio, patreon, registro
+from .views import index, info, portafolio, patreon, registro, referencia
 
 urlpatterns = [
     path('index', index, name="index"),
     path('index/info', info, name="info"),
     path('index/portafolio', portafolio, name="portafolio"),
     path('index/patreon', patreon, name="patreon"),
-    path('index/registro', registro, name="registro")
-    
+    path('index/registro', registro, name="registro"),
+    path('index/referencia', referencia, name="referencia")
 ]
